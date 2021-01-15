f1b61209fc65d59c21ceaf4ba1b1dc64d88214f1
nodauf <nodauf@krutt.org>
2021-01-15 14:09:50 +0100

    Remove debug information

--- src/controllers/upload.go
+++ src/controllers/upload.go
@@ -24,9 +24,6 @@ func UploadFile(w http.ResponseWriter, r *http.Request) {
 		}
 
 		defer file.Close()
-		fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-		fmt.Printf("File Size: %+v\n", handler.Size)
-		//fmt.Printf("MIME Header: %+v\n", handler.Header)
 
 		// Create file
 		dst, err := os.Create(*Root_folder + r.URL.Path + handler.Filename)
